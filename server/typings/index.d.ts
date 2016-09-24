3c9d91705c5ec76580179387b4fc3b170b11e185
Jason Dent <jason@revinate.com>
2016-09-24 10:04:19 +0200

    update library dependencies

--- server/typings/index.d.ts
+++ server/typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/xregexp/index.d.ts" />
 /// <reference path="modules/chai/index.d.ts" />
