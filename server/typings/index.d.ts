0a02151919cb2d77868d4aff6c9c8cc380bcd99e
Jason Dent <jason@revinate.com>
2016-10-14 11:58:34 +0200

    Update everything to use the latest libraries and typings

--- server/typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/xregexp/index.d.ts" />
-/// <reference path="modules/chai/index.d.ts" />
