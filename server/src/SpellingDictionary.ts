e94545abfd083cb18f21c1ad93d168be49513b35
Jason Dent <jason@Jasons-MacBook-Pro.local>
2016-12-30 17:31:21 +0100

    Remove unused code.

--- server/src/SpellingDictionary.ts
+++ server/src/SpellingDictionary.ts
@@ -42,8 +42,3 @@ export function createSpellingDictionaryRx(words: Rx.Observable<string>): Rx.Pro
     return promise;
 }
 
-function asPromise<T>(thenable: { then(fnOnFulfil: (value: T) => T | Promise<T> | void, fnOnReject?: (reason?: any) => any): any }){
-    return new Promise<T>((resolve, reject) => {
-        thenable.then(resolve, reject);
-    });
-}
