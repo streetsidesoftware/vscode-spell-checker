5effef7f22a87a26204014413cd2542b5950c5c5
Jason Dent <jason@Jasons-MacBook-Pro.local>
2017-01-09 18:53:11 +0100

    Update c sample.

--- server/sampleSourceFiles/sample.c
+++ server/sampleSourceFiles/sample.c
@@ -2,18 +2,18 @@
 int hcf(int n1, int n2);
 int main()
 {
-   int n1, n2;
-   printf("Enter two positive integers: ");
-   scanf("%d %d", &n1, &n2);
+    int n1, n2;
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &n1, &n2);
 
-   printf("G.C.D of %d and %d is %d.", n1, n2, hcf(n1,n2));
-   return 0;
+    printf("G.C.D of %d and %d is %d.", n1, n2, hcf(n1, n2));
+    return 0;
 }
 
 int hcf(int n1, int n2)
 {
     if (n2 != 0)
-       return hcf(n2, n1%n2);
+        return hcf(n2, n1 % n2);
     else
-       return n1;
+        return n1;
 }
