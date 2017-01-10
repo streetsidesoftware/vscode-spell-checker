3e77912d27323941dd1ed9b01ccc581e88846029
Jason Dent <jason@Jasons-MacBook-Pro.local>
2017-01-10 21:21:57 +0100

    Node is no longer included by default.
    The common terms were moved to misc.

--- server/test/LanguageSettings.test.ts
+++ server/test/LanguageSettings.test.ts
@@ -5,11 +5,11 @@ describe('Validate LanguageSettings', () => {
     it('tests merging language settings', () => {
         const sPython = calcSettingsForLanguage(defaultLanguageSettings, 'python');
         expect(sPython.allowCompoundWords).to.be.true;
-        expect((sPython.dictionaries || []).sort()).to.be.deep.equal(['wordsEn', 'companies', 'softwareTerms', 'node', 'python', 'misc'].sort());
+        expect((sPython.dictionaries || []).sort()).to.be.deep.equal(['wordsEn', 'companies', 'softwareTerms', 'python', 'misc'].sort());
 
         const sPhp = calcSettingsForLanguage(defaultLanguageSettings, 'php');
         expect(sPhp.allowCompoundWords).to.be.undefined;
         expect((sPhp.dictionaries || []).sort())
-            .to.be.deep.equal(['wordsEn', 'companies', 'softwareTerms', 'node', 'php', 'html', 'fonts', 'css', 'typescript', 'misc'].sort());
+            .to.be.deep.equal(['wordsEn', 'companies', 'softwareTerms', 'php', 'html', 'fonts', 'css', 'typescript', 'misc'].sort());
     });
 });
