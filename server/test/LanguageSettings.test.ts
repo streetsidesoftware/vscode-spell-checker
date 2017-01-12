b9d2f96485fe40d8f3ed642c6a3ab2ab9d178c8b
Jason Dent <jason@Jasons-MacBook-Pro.local>
2017-01-12 12:41:04 +0100

    Fix the test to use the new local param

--- server/test/LanguageSettings.test.ts
+++ server/test/LanguageSettings.test.ts
@@ -3,13 +3,13 @@ import { calcSettingsForLanguage, defaultLanguageSettings } from '../src/Languag
 
 describe('Validate LanguageSettings', () => {
     it('tests merging language settings', () => {
-        const sPython = calcSettingsForLanguage(defaultLanguageSettings, 'python');
+        const sPython = calcSettingsForLanguage(defaultLanguageSettings, 'python', 'en');
         expect(sPython.allowCompoundWords).to.be.true;
         expect((sPython.dictionaries || []).sort()).to.be.deep.equal(['wordsEn', 'companies', 'softwareTerms', 'python', 'misc'].sort());
 
-        const sPhp = calcSettingsForLanguage(defaultLanguageSettings, 'php');
+        const sPhp = calcSettingsForLanguage(defaultLanguageSettings, 'php', 'en-gb');
         expect(sPhp.allowCompoundWords).to.be.undefined;
         expect((sPhp.dictionaries || []).sort())
-            .to.be.deep.equal(['wordsEn', 'companies', 'softwareTerms', 'php', 'html', 'fonts', 'css', 'typescript', 'misc'].sort());
+            .to.be.deep.equal(['wordsEnGb', 'companies', 'softwareTerms', 'php', 'html', 'fonts', 'css', 'typescript', 'misc'].sort());
     });
 });
