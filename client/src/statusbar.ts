68740f50fad4570f57400e1866c405596d640cee
Jason Dent <jason@streetsidesoftware.nl>
2017-08-24 13:51:58 +0100

    Make the status bar more responsive to changes.

--- client/src/statusbar.ts
+++ client/src/statusbar.ts
@@ -4,25 +4,26 @@ import { workspace, ExtensionContext, window, TextEditor, ThemeColor } from 'vsc
 import * as vscode from 'vscode';
 import { CSpellClient } from './cSpellClient';
 import * as cSpellInfo from './cSpellInfo';
-import { Maybe } from './util';
-import { isSupportedUri } from './uriHelper';
+import { isSupportedUri, isSupportedDoc } from './uriHelper';
 
 
 export function initStatusBar(context: ExtensionContext, client: CSpellClient) {
 
     const sbCheck = window.createStatusBarItem(vscode.StatusBarAlignment.Left);
 
-    function updateStatusBarWithSpellCheckStatus(e: Maybe<TextEditor>) {
-        if (!e || !e.document) return;
-        const document = e.document;
-        if (! isSupportedUri(document.uri)) return;
-        const { uri, languageId = '' } = document;
-        const genOnOffIcon = (on: boolean) => on ? '$(checklist)' : '$(stop)';
+    let lastUri = '';
+
+    function updateStatusBarWithSpellCheckStatus(document?: vscode.TextDocument) {
         sbCheck.color = new ThemeColor('statusBar.foreground');
         sbCheck.text = '$(clock)';
         sbCheck.tooltip = 'cSpell waiting...';
         sbCheck.show();
-        client.isSpellCheckEnabled(e.document)
+        if (!document) return;
+
+        const { uri, languageId = '' } = document;
+        lastUri = uri.toString();
+        const genOnOffIcon = (on: boolean) => on ? '$(checklist)' : '$(stop)';
+        client.isSpellCheckEnabled(document)
             .then((response) => {
                 const { activeTextEditor } = window;
                 const document = activeTextEditor && activeTextEditor.document;
@@ -45,10 +46,8 @@ export function initStatusBar(context: ExtensionContext, client: CSpellClient) {
             });
     }
 
-    function onDidChangeActiveTextEditor(e: TextEditor) {
-        if (!e || !e.document) return;
-        const document = e.document;
-        if (! isSupportedUri(document.uri)) return;
+    function updateStatusBar(doc?: vscode.TextDocument) {
+        const document = isSupportedDoc(doc) ? doc : selectDocument();
         const settings: CSpellUserSettings = workspace.getConfiguration().get('cSpell') as CSpellUserSettings;
         const { enabled, showStatus = true } = settings;
 
@@ -58,7 +57,7 @@ export function initStatusBar(context: ExtensionContext, client: CSpellClient) {
         }
 
         if (enabled) {
-            updateStatusBarWithSpellCheckStatus(e);
+            updateStatusBarWithSpellCheckStatus(document);
         } else {
             sbCheck.text = '$(stop) cSpell';
             sbCheck.tooltip = 'Enable spell checking';
@@ -67,10 +66,39 @@ export function initStatusBar(context: ExtensionContext, client: CSpellClient) {
         }
     }
 
+    function onDidChangeActiveTextEditor(e: TextEditor) {
+        updateStatusBar(e && e.document);
+    }
+
     function onDidChangeConfiguration() {
-        if (window.activeTextEditor) {
-            onDidChangeActiveTextEditor(window.activeTextEditor);
+        updateStatusBar();
+    }
+
+    function isViableEditor(e?: TextEditor) {
+        if (!e) return false;
+        const document = e.document;
+
+        return document &&
+            document.uri &&
+            isSupportedUri(document.uri);
+    }
+
+    function selectDocument() {
+        if (isViableEditor(window.activeTextEditor)) {
+            return window.activeTextEditor!.document;
         }
+
+        const docs = workspace.textDocuments
+            .filter(isSupportedDoc);
+
+        if (lastUri) {
+            const candidate = docs
+                .filter(document => document.uri.toString() === lastUri)
+                .shift();
+            if (candidate) return candidate;
+        }
+
+        return docs.shift();
     }
 
     sbCheck.text = '$(clock)';
@@ -79,6 +107,7 @@ export function initStatusBar(context: ExtensionContext, client: CSpellClient) {
     context.subscriptions.push(
         window.onDidChangeActiveTextEditor(onDidChangeActiveTextEditor),
         workspace.onDidChangeConfiguration(onDidChangeConfiguration),
+        workspace.onDidCloseTextDocument(onDidChangeConfiguration),
         sbCheck
     );
 
--- client/src/uriHelper.ts
+++ client/src/uriHelper.ts
@@ -4,7 +4,10 @@ import * as vscode from 'vscode';
 export const supportedSchemes = ['file', 'untitled'];
 export const setOfSupportedSchemes = new Set(supportedSchemes);
 
-export function isSupportedUri(uri: vscode.Uri): boolean {
-    return setOfSupportedSchemes.has(uri.scheme);
+export function isSupportedUri(uri?: vscode.Uri): boolean {
+    return !!uri && setOfSupportedSchemes.has(uri.scheme);
 }
 
+export function isSupportedDoc(doc?: vscode.TextDocument): boolean {
+    return !!doc && !doc.isClosed && isSupportedUri(doc.uri);
+}
