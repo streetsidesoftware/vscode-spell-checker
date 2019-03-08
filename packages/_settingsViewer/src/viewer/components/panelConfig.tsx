1b89173ceb6c84aada59e5af54eb3929d8834dba
Jason Dent <jason@streetsidesoftware.nl>
2019-03-08 13:10:26 +0100

    Work towards selecting the folder.

--- packages/_settingsViewer/src/viewer/components/panelConfig.tsx
+++ packages/_settingsViewer/src/viewer/components/panelConfig.tsx
@@ -4,18 +4,28 @@ import { AppState } from '../AppState';
 import { ConfigTarget } from '../../api/settings';
 import { SectionLanguage } from './sectionLanguage';
 import { SectionFiletypes } from './sectionFiletypes';
+import Select from '@material/react-select';
 
 @observer
 export class PanelConfig extends React.Component<{appState: AppState, target: ConfigTarget}, {}> {
     render() {
         const appState = this.props.appState;
         const target = this.props.target;
-        const config = appState.settings.configs[target];
+        const settings = appState.settings;
+        const config = settings.configs[target];
         if (!config) {
             return <div></div>
         }
+        const workspace = settings.workspace;
+        const workspaceFolders = workspace && workspace.workspaceFolders || [];
+        const options = workspaceFolders.map(folder => folder.name);
         return (
             <div>
+                <Select className='select_folder'
+                    label='Folder'
+                    options={options}
+                    value={options[0]}
+                />
                 <SectionLanguage appState={appState} target={target}></SectionLanguage>
                 <SectionFiletypes appState={appState} target={target}></SectionFiletypes>
             </div>
