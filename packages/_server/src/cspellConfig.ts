9c755322ab2b1fd06ea7d987af12bc702f1e7a4d
Jason Dent <jason@streetsidesoftware.nl>
2020-02-22 16:08:39 +0100

    refactor: Document `SpellCheckerSettings`

--- packages/_server/src/cspellConfig.ts
+++ packages/_server/src/cspellConfig.ts
@@ -4,17 +4,85 @@ import * as cspell from 'cspell-lib';
 export { LanguageSetting, DictionaryDefinition } from 'cspell-lib';
 
 export interface SpellCheckerSettings {
+    /**
+     * The limit in K-Bytes to be checked in a file.
+     * @scope resource
+     * @default 500
+     */
     checkLimit?: number;
-    diagnosticLevel?: string;
-    // By default the allowed schemas are ['file', 'untitled']
+
+    /**
+     * @default "Information"
+     * @scope resource
+     * @description Issues found by the spell checker are marked with a Diagnostic Severity Level. This affects the color of squiggle.
+     */
+    diagnosticLevel?: 'Error' | 'Warning' | 'Information' | 'Hint';
+
+    /**
+     * Control which file schemas will be checked for spelling (VS Code must be restarted for this setting to take effect).
+     * @scope resource
+     * @default ["file", "untitled"]
+     */
     allowedSchemas?: string[];
+
+    /**
+     * Set the Debug Level for logging messages.
+     * @scope window
+     * @default "Error"
+     */
     logLevel?: 'None' | 'Error' | 'Warning' | 'Information' | 'Debug';
+
     // Show the spell checker status on the status bar.
+    /**
+     * Display the spell checker status on the status bar.
+     * @scope application
+     * @default true
+     */
     showStatus?: boolean;
-    // Delay between changes before running the spell checker.
+
+    /**
+     * Delay in ms after a document has changed before checking it for spelling errors.
+     * @scope application
+     * @default 50
+     */
     spellCheckDelayMs?: number;
+
+    /**
+     * Use Rename when fixing spelling issues.
+     * @scope application
+     * @default true
+     */
+    fixSpellingWithRenameProvider?: boolean;
+    /**
+     * Show Spell Checker actions in Editor Context Menu
+     * @scope application
+     * @default true
+     */
     showCommandsInEditorContextMenu?: boolean;
+
+
+    /**
+     * @title File Types to Check
+     * @scope resource
+     * @uniqueItems true
+     * @pattern ^!?[\w_\-]+$
+     * @markdownDescription
+     * Enable / Disable checking file types (languageIds).
+     * These are in additional to the file types specified by `cSpell.enabledLanguageIds`.
+     * To disable a language, prefix with `!` as in `!json`,
+     *
+     * Example:
+     * ```
+     * jsonc       // enable checking for jsonc
+     * !json       // disable checking for json
+     * kotlin      // enable checking for kotlin
+     * ```
+     */
+    enableFiletypes?: string[];
+
 }
 
 export interface CSpellUserSettingsWithComments extends cspell.CSpellUserSettingsWithComments, SpellCheckerSettings {}
 export interface CSpellUserSettings extends cspell.CSpellSettings, SpellCheckerSettings {}
+
+export type SpellCheckerSettingsProperties = keyof SpellCheckerSettings;
