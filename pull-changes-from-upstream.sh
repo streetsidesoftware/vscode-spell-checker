7d8fd6adae108f4b1bcf098b5400a8de3b6b07d2
Jason Dent <jason@streetsidesoftware.nl>
2020-10-31 10:11:29 +0100

    ci: update README

--- pull-changes-from-upstream.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Add upstream Git repository
-# git remote add upstream https://github.com/streetsidesoftware/vscode-spell-checker.git
-
-# Merge latest changes from the upstream Git repository
-git fetch upstream
-git checkout master
-git merge upstream/master
