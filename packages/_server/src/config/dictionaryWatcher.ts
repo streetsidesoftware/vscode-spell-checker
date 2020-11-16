6949ed522fa43a5ad3177a49a9f63c55cea6382c
Jason Dent <jason@streetsidesoftware.nl>
2020-11-16 19:14:47 +0100

    ci: add tests for DictionaryWatcher

--- packages/_server/src/config/dictionaryWatcher.ts
+++ packages/_server/src/config/dictionaryWatcher.ts
@@ -1,12 +1,16 @@
 import { FileWatcher } from '../utils/fileWatcher';
 import { CSpellUserSettings } from 'cspell-lib';
+import { Disposable } from 'vscode-languageserver';
 
 export type Listener = (eventType?: string, filename?: string) => void;
 
-export class DictionaryWatcher extends FileWatcher {
-    constructor() {
-        super();
-    }
+export class DictionaryWatcher implements Disposable {
+    private fileWatcher = new FileWatcher();
+
+    readonly dispose = (): void => {
+        this.clear();
+        this.fileWatcher.dispose();
+    };
 
     processSettings(finalizedSettings: CSpellUserSettings): void {
         // Only watch used dictionaries.
@@ -14,6 +18,14 @@ export class DictionaryWatcher extends FileWatcher {
         finalizedSettings.dictionaries
             ?.map((name) => defs.get(name))
             .filter((s): s is string => !!s)
-            .forEach((file) => this.addFile(file));
+            .forEach((file) => this.fileWatcher.addFile(file));
+    }
+
+    clear(): void {
+        this.fileWatcher.clear();
+    }
+
+    listen(fn: Listener): Disposable {
+        return this.fileWatcher.listen(fn);
     }
 }
