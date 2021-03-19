a79557455ed5e832b2b6303b616ba93bf4aec50d
Jason Dent <jason@streetsidesoftware.nl>
2021-03-19 16:39:27 +0100

    dev: Move to Yarn

--- packages/_server/src/utils/util.ts
+++ packages/_server/src/utils/util.ts
@@ -14,3 +14,11 @@ export function uniqueFilter<T>(extractFn?: (v: T) => T): (v: T) => boolean {
 export function isDefined<T>(v: T | undefined | null): v is T {
     return v !== undefined && v !== null;
 }
+
+/**
+ * Escape a string so it can be used in a RegExp.
+ * @param s - string to escape
+ */
+export function escapeRegExp(s: string): string {
+    return s.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d');
+}
