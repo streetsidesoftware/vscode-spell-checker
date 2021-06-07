275f07e4e11861b4141e100a1814bd3f9c351e64
Jason Dent <Jason3S@users.noreply.github.com>
2021-06-07 12:23:27 +0200

    fix: Verify Yarn2 projects work (#925)
    * fix: Verify Yarn2 projects work
    
    * ci: Update packages

---
+++ fixtures/workspaces/yarn2/yarn2-test-science/cspell.config.js
@@ -0,0 +1,13 @@
+'use strict';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('./.pnp.js').setup();
+
+/** @type { import("@cspell/cspell-types").CSpellUserSettings } */
+const cspell = {
+    description: 'Make cspell Yarn 2 PNP aware',
+    import: [
+        '@cspell/dict-scientific-terms-us/cspell-ext.json'
+    ]
+};
+
+module.exports = cspell;
