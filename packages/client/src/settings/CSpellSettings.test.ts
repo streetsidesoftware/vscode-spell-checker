e840a28def47446d69d0709dfbe4ed73464eb5eb
Jason Dent <jason@streetsidesoftware.nl>
2021-06-25 21:37:42 +0200

    dev: add function to add words to a dictionary

--- packages/client/src/settings/CSpellSettings.test.ts
+++ packages/client/src/settings/CSpellSettings.test.ts
@@ -3,7 +3,7 @@ import * as CSS from './CSpellSettings';
 import { unique } from '../util';
 import { CSpellUserSettings } from '.';
 import { Uri } from 'vscode';
-import { fsRemove, getPathToTemp, getUriToSample, writeFile } from '../test/helpers';
+import { fsRemove, getPathToTemp, getUriToSample, writeFile, readFile, mkdirp } from '../test/helpers';
 
 describe('Validate CSpellSettings functions', () => {
     const filenameSampleCSpellFile = getUriToSample('cSpell.json');
@@ -49,7 +49,7 @@ describe('Validate CSpellSettings functions', () => {
         const word = 'word';
         const filename = getPathToTemp('addWordToSettingsAndUpdate/cspell.json');
         await writeFile(filename, sampleJsonConfig);
-        const r = await CSS.addWordToSettingsAndUpdate(filename, word);
+        const r = await CSS.addWordsToSettingsAndUpdate(filename, word);
         expect(r.words).toEqual(expect.arrayContaining([word]));
         expect(await readSettings(filename)).toEqual(r);
     });
@@ -132,7 +132,57 @@ describe('Validate CSpellSettings functions', () => {
         const uriCfg = Uri.parse(uri);
         expect(CSS.isUpdateSupportedForConfigFileFormat(uriCfg)).toBe(expected);
     });
+
+    test('addWordsToCustomDictionary', async () => {
+        const dictUri = getPathToTemp('addWordsToCustomDictionary/dict.txt');
+        const words1 = ['one', 'two', 'three'];
+        const words2 = ['alpha', 'beta', 'delta', 'zeta', 'one'];
+        const expected = [...['alpha', 'beta', 'delta', 'zeta', 'one', 'two', 'three'].sort(), ''].join('\n');
+        await expect(CSS.addWordsToCustomDictionary(words1, dictUri)).resolves.toBeUndefined();
+        expect(readFile(dictUri)).resolves.toBe([...words1].sort().join('\n') + '\n');
+        await expect(CSS.addWordsToCustomDictionary(words2, dictUri)).resolves.toBeUndefined();
+        expect(readFile(dictUri)).resolves.toBe(expected);
+    });
+
+    test.each`
+        file              | error
+        ${''}             | ${e(sm(/Failed to add words to dictionary .*, unsupported format./))}
+        ${'words.txt.gz'} | ${e(sm(/words.txt.gz.*unsupported format./))}
+        ${'cspell.json'}  | ${e(sm(/cspell.json.*unsupported format./))}
+    `('addWordsToCustomDictionary_failures $file', async ({ file, error }) => {
+        const pathUri = getPathToTemp('addWordsToCustomDictionary_failures');
+        await mkdirp(pathUri);
+        const dictUri = Uri.joinPath(pathUri, file);
+        const words = ['one', 'two', 'three'];
+        await expect(CSS.addWordsToCustomDictionary(words, dictUri)).rejects.toEqual(error);
+    });
+
+    test.each`
+        file           | error
+        ${'words.txt'} | ${e(sm(/Failed to add words to dictionary.*EISDIR/))}
+    `('addWordsToCustomDictionary_cannot_write $file', async ({ file, error }) => {
+        const pathUri = getPathToTemp('addWordsToCustomDictionary_cannot_write');
+        const dictUri = Uri.joinPath(pathUri, file);
+        // Make the file into a directory to force the error.
+        await mkdirp(dictUri);
+        const words = ['one', 'two', 'three'];
+        await expect(CSS.addWordsToCustomDictionary(words, dictUri)).rejects.toEqual(error);
+    });
 });
+
+function oc(...args: Parameters<typeof expect.objectContaining>) {
+    return expect.objectContaining(...args);
+}
+
+function sm(...args: Parameters<typeof expect.stringMatching>) {
+    return expect.stringMatching(...args);
+}
+
+function e(message: string) {
+    return oc({
+        message,
+    });
+}
 
 const sampleJSConfig = `
 module.exports = {
