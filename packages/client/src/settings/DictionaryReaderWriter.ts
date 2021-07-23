b45316d98bba381752bfb198d373a8dfa57826b1
Jason Dent <jason@streetsidesoftware.nl>
2021-07-23 23:29:04 +0200

    dev: Unify updating Dictionaries

---
+++ packages/client/src/settings/DictionaryReaderWriter.ts
@@ -0,0 +1,107 @@
+import { CSpellUserSettings } from '../server';
+import { uriToName } from 'common-utils/uriHelper.js';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import { Uri } from 'vscode';
+import { unique } from '../util';
+import { ConfigRepository } from './configRepository';
+import { CustomDictDef } from './CSpellSettings';
+
+const regIsSupportedCustomDictionaryFormat = /\.txt$/i;
+
+export interface Dictionary {
+    /** Name of dictionary */
+    readonly name: string;
+    addWords: (words: string[]) => Promise<void>;
+    removeWords: (words: string[]) => Promise<void>;
+}
+
+export interface DictionaryFile extends Dictionary {
+    /** Uri of the dictionary file */
+    readonly uri: Uri;
+}
+
+class DictionaryFileInstance implements DictionaryFile {
+    readonly name: string;
+    constructor(readonly uri: Uri, name?: string) {
+        this.name = name ?? uriToName(uri);
+    }
+
+    addWords(words: string[]): Promise<void> {
+        return addWordsToCustomDictionary(words, this);
+    }
+
+    removeWords(words: string[]): Promise<void> {
+        return updateWordInCustomDictionary(removeWordsFn(words), this);
+    }
+}
+
+const configKeyWords = ['words'] as const;
+
+class DictionaryInConfig implements Dictionary {
+    readonly name: string;
+    constructor(readonly rep: ConfigRepository) {
+        this.name = rep.name;
+    }
+
+    addWords(words: string[]): Promise<void> {
+        return this.rep.update(updateConfigFn(addWordsFn(words)), configKeyWords);
+    }
+
+    removeWords(words: string[]): Promise<void> {
+        return this.rep.update(updateConfigFn(removeWordsFn(words)), configKeyWords);
+    }
+}
+
+export function createDictionaryFileInstance(uri: Uri, name?: string): DictionaryFile {
+    return new DictionaryFileInstance(uri, name);
+}
+
+export function createDictionaryInConfig(rep: ConfigRepository): Dictionary {
+    return new DictionaryInConfig(rep);
+}
+
+type UpdateWords = (lines: string[]) => string[];
+
+function updateConfigFn(updateFn: UpdateWords): (cfg: CSpellUserSettings) => CSpellUserSettings {
+    return (cfg) => {
+        const words = updateFn(cfg.words ?? []);
+        return { words };
+    };
+}
+
+function addWordsFn(words: string[]): (lines: string[]) => string[] {
+    return (lines) => lines.concat(words);
+}
+
+function removeWordsFn(words: string[]): (lines: string[]) => string[] {
+    return (lines) => {
+        const current = new Set(lines);
+        for (const w of words) {
+            current.delete(w);
+        }
+        return [...current];
+    };
+}
+
+export async function addWordsToCustomDictionary(words: string[], dict: CustomDictDef): Promise<void> {
+    return updateWordInCustomDictionary(addWordsFn(words), dict);
+}
+
+async function updateWordInCustomDictionary(updateFn: (words: string[]) => string[], dict: CustomDictDef): Promise<void> {
+    const fsPath = dict.uri.fsPath;
+    if (!regIsSupportedCustomDictionaryFormat.test(fsPath)) {
+        return Promise.reject(new Error(`Failed to add words to dictionary "${dict.name}", unsupported format: "${dict.uri.fsPath}".`));
+    }
+    try {
+        const data = await fs.readFile(fsPath, 'utf8').catch(() => '');
+
+        const lines = unique(updateFn(data.split(/\r?\n/g)))
+            .filter((a) => !!a)
+            .sort();
+        await fs.mkdirp(path.dirname(fsPath));
+        await fs.writeFile(fsPath, lines.join('\n').concat('\n'));
+    } catch (e) {
+        return Promise.reject(new Error(`Failed to add words to dictionary "${dict.name}", ${e.toString()}`));
+    }
+}
