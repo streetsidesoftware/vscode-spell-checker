d9b27c8006aa83189b1b6ca1f2436ad6afff6d2d
Jason Dent <jason@streetsidesoftware.nl>
2021-08-02 23:00:14 +0200

    dev: add friendly links to custom dictionaries in UI

--- packages/client/src/infoViewer/infoHelper.test.ts
+++ packages/client/src/infoViewer/infoHelper.test.ts
@@ -1,6 +1,9 @@
 import { __testing__ } from './infoHelper';
+import * as cspell from 'cspell-lib';
+import { isDefined } from '../util';
 
-const { splitBangPrefix, calcEnableLang, applyEnableFiletypesToEnabledLanguageIds } = __testing__;
+const { applyEnableFiletypesToEnabledLanguageIds, calcEnableLang, extractDictionariesFromConfig, normalizeLocales, splitBangPrefix } =
+    __testing__;
 
 describe('infoHelper', () => {
     test.each`
@@ -35,4 +38,52 @@ describe('infoHelper', () => {
     `('applyEnableFiletypesToEnabledLanguageIds $languageIds, $enableFiletypes', ({ languageIds, enableFiletypes, expected }) => {
         expect(applyEnableFiletypesToEnabledLanguageIds(languageIds, enableFiletypes)).toEqual(expected);
     });
+
+    test.each`
+        locale                | expected
+        ${''}                 | ${[]}
+        ${'en-US, en_GB'}     | ${['en-US', 'en_GB']}
+        ${'en-US;nl'}         | ${['en-US', 'nl']}
+        ${['en-US;nl', 'es']} | ${['en-US', 'nl', 'es']}
+    `('normalizeLocales $locale', ({ locale, expected }) => {
+        expect(normalizeLocales(locale)).toEqual(expected);
+    });
+
+    test('extractDictionariesFromConfig', async () => {
+        const cfg = await sampleCSpellSettings();
+        expect(extractDictionariesFromConfig(cfg)).toEqual(
+            expect.arrayContaining([
+                {
+                    description: 'American English Dictionary',
+                    languageIds: [],
+                    locales: ['en', 'en-US'],
+                    name: 'en_us',
+                },
+                {
+                    description: 'Lorem-ipsum dictionary for cspell.',
+                    languageIds: [],
+                    locales: ['lorem', 'lorem-ipsum'],
+                    name: 'lorem-ipsum',
+                },
+                {
+                    description: undefined,
+                    languageIds: [],
+                    locales: [],
+                    name: 'cspell-words',
+                    uri: expect.stringContaining('cspell-words.txt'),
+                    uriName: expect.stringContaining('cspell-words.txt'),
+                },
+            ])
+        );
+    });
+
+    test('extractDictionariesFromConfig undefined', async () => {
+        expect(extractDictionariesFromConfig(undefined)).toEqual([]);
+    });
 });
+
+async function sampleCSpellSettings() {
+    const localCfg = await cspell.searchForConfig(__filename);
+    const settings = [cspell.getDefaultSettings(), cspell.getGlobalSettings(), localCfg].filter(isDefined);
+    return cspell.mergeSettings(settings[0], ...settings.slice(1));
+}
