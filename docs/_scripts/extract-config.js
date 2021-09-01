71e641e3c35eb45748e1d6264271d0f7fc82972d
Jason Dent <jason@streetsidesoftware.nl>
2021-09-01 13:47:39 +0200

    lint: extract-config.js

--- docs/_scripts/extract-config.js
+++ docs/_scripts/extract-config.js
@@ -6,11 +6,9 @@ const config = package.contributes.configuration.properties;
 
 const compare = new Intl.Collator().compare;
 
-const entries = Object.entries(config)
-    .sort((a, b) => compare(a[0], b[0]));
+const entries = Object.entries(config).sort((a, b) => compare(a[0], b[0]));
 
-const activeEntries = entries
-.filter(([, value]) => !value.deprecationMessage);
+const activeEntries = entries.filter(([, value]) => !value.deprecationMessage);
 
 const doc = `---
 # Note this document is automatically generated.
@@ -39,11 +37,11 @@ ${configDefinitions(entries)}
 console.log(doc);
 
 function configTable(entries) {
-    return `
+  return `
 | Command | Description |
 | ------- | ----------- |
 ${entries.map(tableEntry).join('\n')}
-`
+`;
 }
 
 /**
@@ -52,10 +50,12 @@ ${entries.map(tableEntry).join('\n')}
  * @returns
  */
 function tableEntry([key, value]) {
-    const description = value.title ||
-        value.description?.replace(/\n/g, '<br>') ||
-        value.markdownDescription?.replace(/\n[\s\S]*/g, ' ') || '';
-    return `| [\`${key}\`](#${key.toLowerCase().replace(/\W/g, '')}) | ${shortenLine(description, 50)} |`;
+  const description =
+    value.title ||
+    value.description?.replace(/\n/g, '<br>') ||
+    value.markdownDescription?.replace(/\n[\s\S]*/g, ' ') ||
+    '';
+  return `| [\`${key}\`](#${key.toLowerCase().replace(/\W/g, '')}) | ${shortenLine(description, 50)} |`;
 }
 
 /**
@@ -65,22 +65,21 @@ function tableEntry([key, value]) {
  * @returns
  */
 function shortenLine(line, len) {
-    const lines = line.split('<br>');
-    if (lines.length > 1) return shortenLine(lines[0], len);
-    if (line.length <= len) return line;
+  const lines = line.split('<br>');
+  if (lines.length > 1) return shortenLine(lines[0], len);
+  if (line.length <= len) return line;
 
-    const isSpace = /\s/;
+  const isSpace = /\s/;
 
-    let i = len;
-    while (i < line.length && !isSpace.test(line[i])) {
-        ++i;
-    }
-    return i < line.length ? line.slice(0, i) + '...' : line;
+  let i = len;
+  while (i < line.length && !isSpace.test(line[i])) {
+    ++i;
+  }
+  return i < line.length ? line.slice(0, i) + '...' : line;
 }
 
-
 function configDefinitions(entries) {
-    return entries.map(definition).join('\n')
+  return entries.map(definition).join('\n');
 }
 
 /**
@@ -89,27 +88,24 @@ function configDefinitions(entries) {
  * @returns
  */
 function definition(entry) {
-    const [key, value] = entry;
-    const description =
-        value.markdownDescription ||
-        value.description ||
-        value.title ||
-        ''
-        ;
-    const defaultValue = formatDefaultValue(value.default);
+  const [key, value] = entry;
+  const description = value.markdownDescription || value.description || value.title || '';
+  const defaultValue = formatDefaultValue(value.default);
 
-    const title = value.title ? `-- ${value.title}` : '';
-    let name = '`' + key + '`';
-    if (value.deprecationMessage) {
-        name = '~~' + name + '~~';
-    }
+  const title = value.title ? `-- ${value.title}` : '';
+  let name = '`' + key + '`';
+  if (value.deprecationMessage) {
+    name = '~~' + name + '~~';
+  }
 
-    const deprecationMessage = value.deprecationMessage ? `
+  const deprecationMessage = value.deprecationMessage
+    ? `
 Deprecation Message
 : ${value.deprecationMessage.replace(/\n/g, '\n    ')}
-` : '';
+`
+    : '';
 
-    return `
+  return `
 ### ${name}
 
 Name
@@ -130,35 +126,35 @@ Default
 `;
 }
 
-
 function formatDefaultValue(value) {
-    if (value === undefined) return '_- none -_';
+  if (value === undefined) return '_- none -_';
 
-    if (Array.isArray(value)) {
-        return '[ ' + value.map(formatDefaultValue).join(', ') + ' ]'
-    }
+  if (Array.isArray(value)) {
+    return '[ ' + value.map(formatDefaultValue).join(', ') + ' ]';
+  }
 
-    return '_`' + JSON.stringify(value) + '`_';
+  return '_`' + JSON.stringify(value) + '`_';
 }
 
 function extractType(def) {
-    if (def.type === 'array') return extractType(def.items) + '[]';
+  if (def.type === 'array') return extractType(def.items) + '[]';
 
-    if (def.enum) {
-        return `( ${def.enum.map(v => '`' + JSON.stringify(v) + '`').join(' \\| ')} )`
-    }
+  if (def.enum) {
+    return `( ${def.enum.map((v) => '`' + JSON.stringify(v) + '`').join(' \\| ')} )`;
+  }
 
-    return def.type || '';
+  return def.type || '';
 }
 
 function extractEnumDescriptions(def) {
-    if (!def.enumDescriptions || !def.enum) return '';
+  if (!def.enumDescriptions || !def.enum) return '';
 
-    const defs = def.enum
+  const defs = def.enum
     .map((e, i) => [e, def.enumDescriptions[i] || '_No description_'])
-    .map(([e, d]) => (`| \`${e}\` | ${d.replace(/\n/g, '<br>')} |`)).join('\n')
+    .map(([e, d]) => `| \`${e}\` | ${d.replace(/\n/g, '<br>')} |`)
+    .join('\n');
 
-    return `
+  return `
 
 | Value | Description |
 | ----- | ----------- |
@@ -167,7 +163,7 @@ ${defs}
 }
 
 function formatType(def) {
-    const type = extractType(def);
-    const enumDefs = extractEnumDescriptions(def);
-    return type + enumDefs;
+  const type = extractType(def);
+  const enumDefs = extractEnumDescriptions(def);
+  return type + enumDefs;
 }
