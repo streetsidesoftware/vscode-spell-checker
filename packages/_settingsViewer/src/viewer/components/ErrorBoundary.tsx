a97dd042264f09972de75d1b152a6eb3c33ddcba
Jason Dent <jason@streetsidesoftware.nl>
2021-09-18 16:48:41 +0200

    dev: fix lint issues

--- packages/_settingsViewer/src/viewer/components/ErrorBoundary.tsx
+++ packages/_settingsViewer/src/viewer/components/ErrorBoundary.tsx
@@ -1,32 +1,32 @@
 import * as React from 'react';
 
 interface Props {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 interface State {
-  hasError: boolean;
+    hasError: boolean;
 }
 
 export class ErrorBoundary extends React.Component<Props, State> {
-  public state: State = {
-    hasError: false
-  };
+    public state: State = {
+        hasError: false,
+    };
 
-  public static getDerivedStateFromError(_: Error): State {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true };
-  }
-
-  public componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
-    console.error('Uncaught error:', error, errorInfo);
-  }
-
-  public render(): React.ReactNode {
-    if (this.state.hasError) {
-      return <h1>Sorry.. there was an error</h1>;
+    public static getDerivedStateFromError(_: Error): State {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
     }
 
-    return this.props.children;
-  }
+    public componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+        console.error('Uncaught error:', error, errorInfo);
+    }
+
+    public render(): React.ReactNode {
+        if (this.state.hasError) {
+            return <h1>Sorry.. there was an error</h1>;
+        }
+
+        return this.props.children;
+    }
 }
