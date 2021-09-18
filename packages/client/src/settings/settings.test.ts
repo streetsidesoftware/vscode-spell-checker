b745e39b38480cf24f2e912dbce7f133c249a5b8
Jason Dent <Jason3S@users.noreply.github.com>
2021-09-18 09:29:27 +0200

    fix: Fix Enable / Disable Locale matching logic. (#1318)

--- packages/client/src/settings/settings.test.ts
+++ packages/client/src/settings/settings.test.ts
@@ -1,7 +1,28 @@
-import { hasWorkspaceLocation } from './settings';
+import { hasWorkspaceLocation, addLocaleToCurrentLocale, removeLocaleFromCurrentLocale } from './settings';
 
 describe('Validate settings.ts', () => {
     test('hasWorkspaceLocation', () => {
         expect(hasWorkspaceLocation()).toBe(false);
+    });
+
+    test.each`
+        locale              | current | expected
+        ${'en'}             | ${'en'} | ${'en'}
+        ${'nl_nl'}          | ${'en'} | ${'en,nl-NL'}
+        ${'nl,nl_nl,nl-Nl'} | ${'en'} | ${'en,nl,nl-NL'}
+    `('addLocaleToCurrentLocale $locale + $current => $expected', ({ locale, current, expected }) => {
+        expect(addLocaleToCurrentLocale(locale, current)).toBe(expected);
+    });
+
+    test.each`
+        locale        | current       | expected
+        ${'en'}       | ${'en'}       | ${undefined}
+        ${'nl_nl'}    | ${'en'}       | ${'en'}
+        ${'nl_nl'}    | ${'en,nl-NL'} | ${'en'}
+        ${'nl,nl_nl'} | ${'en,nl'}    | ${'en'}
+        ${'en;nl_nl'} | ${'en,nl'}    | ${'nl'}
+        ${'en;nl_nl'} | ${'en, nl'}   | ${'nl'}
+    `('removeLocaleFromCurrentLocale $current - $locale => $expected', ({ locale, current, expected }) => {
+        expect(removeLocaleFromCurrentLocale(locale, current)).toBe(expected);
     });
 });
