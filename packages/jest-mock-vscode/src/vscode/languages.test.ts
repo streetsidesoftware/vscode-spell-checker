a7aeed810e5e0931b5cb8735f644340506ea17d6
Jason Dent <jason@streetsidesoftware.nl>
2022-08-07 14:09:07 +0200

    fix: Remove package jest-mock-vscode
    It was moved to [streetsidesoftware/jest-mock-vscode: VS Code Mock for jest](https://github.com/streetsidesoftware/jest-mock-vscode)

--- packages/jest-mock-vscode/src/vscode/languages.test.ts
+++
@@ -1,9 +0,0 @@
-import { languages } from './languages';
-
-describe('languages', () => {
-    test('getLanguages', async () => {
-        const langs = await languages.getLanguages();
-        expect(langs).toBeDefined();
-        expect(langs.length).toBeGreaterThan(1);
-    });
-});
