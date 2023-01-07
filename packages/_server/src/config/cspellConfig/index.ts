59c04bf9853179ffe7590eccd862c46b5c01a3ff
Jason Dent <jason@streetsidesoftware.nl>
2023-01-07 14:18:02 +0100

    fix: make sure `noSuggest` custom dictionaries are possible.

---
+++ packages/_server/src/config/cspellConfig/index.ts
@@ -0,0 +1,22 @@
+export type {
+    CustomDictionaryScope,
+    DictionaryDefinition,
+    DictionaryDefinitionCustom,
+    DictionaryFileTypes,
+    LanguageSetting,
+} from '@cspell/cspell-types';
+export type {
+    AllSpellCheckerSettingsInVSCode,
+    CSpellUserSettings,
+    CustomDictionaries,
+    CustomDictionariesDictionary,
+    CustomDictionary,
+    CustomDictionaryAugmentExistingDictionary,
+    CustomDictionaryEntry,
+    CustomDictionaryWithScope,
+    SpellCheckerSettings,
+    SpellCheckerSettingsProperties,
+    SpellCheckerSettingsVSCode,
+    SpellCheckerSettingsVSCodeBase,
+    SpellCheckerSettingsVSCodePropertyKeys,
+} from './cspellConfig';
