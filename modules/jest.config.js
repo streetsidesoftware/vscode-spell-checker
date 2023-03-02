219d9a45984157836f9d640683a54e8f9a22cde3
Jason Dent <jason@streetsidesoftware.nl>
2023-03-02 14:55:18 +0100

    Keep trying to use "path"

--- modules/jest.config.js
+++ modules/jest.config.js
@@ -5,6 +5,10 @@ const config = {
     ...rootConfig,
     roots: ['.'],
     testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+    transform: {
+        // '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.base.json' }],
+        '^.+/_server/.+\\.tsx?$': ['ts-jest', { tsconfig: '_server/tsconfig.json' }],
+    },
 };
 
 module.exports = config;
