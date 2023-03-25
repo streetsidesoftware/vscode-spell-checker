1f1a6d0c9b253af9de8227a984cad1baa975066d
Jason Dent <jason@streetsidesoftware.nl>
2023-03-25 13:09:08 +0100

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
+        outputTruncateLength: 180,
         // reporters: 'verbose',
         coverage: {
             // enabled: true,
