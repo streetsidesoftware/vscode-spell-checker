e932f42fe254d907dc0b79098137e657e6a6b100
Jason Dent <jason@streetsidesoftware.nl>
2023-08-24 17:10:45 +0200

    Use esbuild to create server

---
+++ packages/client/build.mjs
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+/* eslint-disable node/no-extraneous-import */
+import * as esbuild from 'esbuild';
+import { fileURLToPath } from 'url';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+const argv = yargs(hideBin(process.argv)).argv;
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const minify = argv['minify'] ?? argv['production'] ?? false;
+const watchMode = argv['watch'] || false;
+
+/**
+ *
+ * @param {import('esbuild').BuildOptions} options
+ */
+function build(options) {
+    return esbuild.build(options);
+}
+
+/**
+ *
+ * @param {import('esbuild').BuildOptions} options
+ */
+async function watch(options) {
+    const ctx = await esbuild.context(options);
+    return ctx.watch();
+}
+
+/**
+ * @type {import('esbuild').BuildOptions}
+ */
+const optionsBase = {
+    absWorkingDir: __dirname,
+    bundle: true,
+    minify,
+    platform: 'node',
+    sourcemap: true,
+    external: ['vscode'],
+};
+
+async function buildAll() {
+    // Note: cjs is the only possible option at this moment.
+
+    /** @type {import('esbuild').BuildOptions[]} */
+    const configs = [
+        {
+            ...optionsBase,
+            entryPoints: ['src/extension.ts'],
+            outfile: 'dist/extension.js',
+        },
+        {
+            ...optionsBase,
+            entryPoints: ['src/extensionApi.ts'],
+            outfile: 'dist/extensionApi.js',
+        },
+    ];
+
+    const builds = configs.map(watchMode ? watch : build);
+    await Promise.all(builds);
+}
+
+buildAll();
