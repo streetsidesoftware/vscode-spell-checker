c8d0821256a8d9e9ebec6834fc228f026defe4a3
Jason Dent <jason@streetsidesoftware.nl>
2023-10-08 10:39:56 +0200

    Add the new api

---
+++ packages/_server/src/api/api.ts
@@ -0,0 +1,87 @@
+import type {
+    ApplyNotificationAPI,
+    ApplyRequestAPI,
+    ClientAPIDef,
+    ClientSideMethods,
+    Logger,
+    MessageConnection,
+    RpcAPI,
+    ServerAPIDef,
+    ServerSideMethods,
+} from 'json-rpc-api';
+import { createClientApi, createServerApi } from 'json-rpc-api';
+
+import type {
+    ConfigurationTarget,
+    GetConfigurationForDocumentRequest,
+    GetConfigurationForDocumentResult,
+    IsSpellCheckEnabledResult,
+    OnSpellCheckDocumentStep,
+    SpellingSuggestionsResult,
+    SplitTextIntoWordsResult,
+    TextDocumentInfo,
+} from './apiModels.js';
+
+/** Requests that can be made to the server */
+export interface ServerRequestsAPI {
+    getConfigurationForDocument(req: GetConfigurationForDocumentRequest): GetConfigurationForDocumentResult;
+    isSpellCheckEnabled(req: TextDocumentInfo): IsSpellCheckEnabledResult;
+    splitTextIntoWords(req: string): SplitTextIntoWordsResult;
+    spellingSuggestions(req: TextDocumentInfo): SpellingSuggestionsResult;
+}
+
+/** Notifications that can be sent to the server */
+export interface ServerNotificationsAPI {
+    notifyConfigChange: () => void;
+    registerConfigurationFile: (path: string) => void;
+}
+
+/**
+ * Requests that can be made from the server to the client(vscode extension)
+ * Note: RPC requests to the client/extension is rare.
+ */
+export interface ClientRequestsAPI {
+    addWordsToVSCodeSettingsFromServer: (words: string[], documentUri: string, target: ConfigurationTarget) => void;
+    addWordsToDictionaryFileFromServer: (words: string[], documentUri: string, dict: { uri: string; name: string }) => void;
+    addWordsToConfigFileFromServer: (words: string[], documentUri: string, config: { uri: string; name: string }) => void;
+}
+
+/** Notifications from the server to the client(vscode extension) */
+export interface ClientNotificationsAPI {
+    onSpellCheckDocument(step: OnSpellCheckDocumentStep): void;
+}
+
+export interface SpellInfoWebviewAPI extends RpcAPI {
+    serverRequests: ApplyRequestAPI<ServerRequestsAPI>;
+    serverNotifications: ApplyNotificationAPI<ServerNotificationsAPI>;
+    clientRequests: ApplyRequestAPI<ClientRequestsAPI>;
+    clientNotifications: ApplyNotificationAPI<ClientNotificationsAPI>;
+}
+
+/**
+ * Used on the server side  to communicate with the client(extension).
+ */
+export interface ServerSideApi extends ServerSideMethods<SpellInfoWebviewAPI> {}
+/**
+ * Used in the client(extension) to communicate with the server.
+ */
+export interface ClientSideApi extends ClientSideMethods<SpellInfoWebviewAPI> {}
+
+export type ServerSideApiDef = ServerAPIDef<SpellInfoWebviewAPI>;
+export type ClientSideApiDef = ClientAPIDef<SpellInfoWebviewAPI>;
+
+export function createServerSideSpellInfoWebviewApi(
+    connection: MessageConnection,
+    api: ServerAPIDef<SpellInfoWebviewAPI>,
+    logger: Logger | undefined,
+): ServerSideApi {
+    return createServerApi(connection, api, logger);
+}
+
+export function createClientSideSpellInfoWebviewApi(
+    connection: MessageConnection,
+    api: ClientAPIDef<SpellInfoWebviewAPI>,
+    logger: Logger | undefined,
+): ClientSideApi {
+    return createClientApi(connection, api, logger);
+}
