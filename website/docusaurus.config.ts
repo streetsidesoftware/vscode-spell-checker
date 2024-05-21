41b910c1c3883e20985994d36354ca8937118ba8
Jason Dent <jason@streetsidesoftware.nl>
2024-05-21 12:44:22 +0200

    chore: fix doc deploy dir

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     url: 'https://streetsidesoftware.com',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/vscode-spell-checker/docs2/',
+    baseUrl: '/vscode-spell-checker/docsV2/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
