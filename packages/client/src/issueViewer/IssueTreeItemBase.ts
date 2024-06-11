7332eea2c3fa61005238c877d5b87a9aed63482b
Jason Dent <jason@streetsidesoftware.nl>
2024-06-11 19:23:55 +0200

    Update IssueTreeItemBase.ts

--- packages/client/src/issueViewer/IssueTreeItemBase.ts
+++ packages/client/src/issueViewer/IssueTreeItemBase.ts
@@ -1,5 +1,6 @@
 import type { ProviderResult, TreeItem } from 'vscode';
 
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export abstract class IssueTreeItemBase {
     abstract getTreeItem(): TreeItem | Promise<TreeItem>;
     abstract getChildren(): ProviderResult<IssueTreeItemBase[]>;
