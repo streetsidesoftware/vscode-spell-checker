01dc92dd8c99344d1d46dd24918357d39a4c6e12
Jason Dent <Jason3S@users.noreply.github.com>
2025-01-05 17:55:07 +0100

    chore: Use `package.vsce` to set `preRelease` (#4003)

--- scripts/set-prerelease-mode.js
+++ scripts/set-prerelease-mode.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 
+// @ts-check
 import fs from 'node:fs/promises';
+
+/**
+ * @template T
+ * @typedef {{ -readonly [K in keyof T]: T[K] }} Mutable<T>
+ */
+
+/**
+ * @typedef {Mutable<import('@vscode/vsce').IPackageOptions>} VSCEPackageOptions
+ */
 
 const pkgUrl = new URL('../package.json', import.meta.url);
 
@@ -16,16 +26,18 @@ async function run() {
     const isPrereleaseMode = process.argv[2] === 'true' || undefined;
 
     const content = JSON.parse(await fs.readFile(pkgUrl, 'utf-8'));
+    const vsce = /** @type {VSCEPackageOptions} */ (content['vsce'] || {});
+    content['vsce'] = vsce;
 
-    if (!!content['prerelease'] === isPrereleaseMode) {
+    if (!!vsce.preRelease === isPrereleaseMode) {
         console.log(`Prerelease mode is already ${isPrereleaseMode}`);
         return;
     }
 
     if (isPrereleaseMode) {
-        content['prerelease'] = true;
+        vsce.preRelease = true;
     } else {
-        delete content['prerelease'];
+        vsce.preRelease = undefined;
     }
 
     await fs.writeFile(pkgUrl, JSON.stringify(content, null, 2) + '\n');
